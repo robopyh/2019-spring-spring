b2874b32a95996349925bd8c8335cc15aa32ec32
Andrei Tretnikov <andrei_tretnikov@epam.com>
2019-06-18 23:40:50 +0300

    Refactor BFPP

--- src/main/java/com/epam/hw/CustomBeanFactoryPostProcessor.java
+++ src/main/java/com/epam/hw/CustomBeanFactoryPostProcessor.java
@@ -1,11 +1,9 @@
 package com.epam.hw;
 
-import com.epam.hw.bean.Doggo;
 import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
-import org.springframework.beans.factory.support.DefaultListableBeanFactory;
-import org.springframework.beans.factory.support.GenericBeanDefinition;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -14,10 +12,7 @@ public class CustomBeanFactoryPostProcessor implements BeanFactoryPostProcessor 
   @Override
   public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory)
       throws BeansException {
-    GenericBeanDefinition beanDefinition = new GenericBeanDefinition();
-    beanDefinition.setBeanClass(Doggo.class);
+    BeanDefinition beanDefinition = beanFactory.getBeanDefinition("doggo");
     beanDefinition.getPropertyValues().add("name", "Persik");
-    ((DefaultListableBeanFactory) beanFactory)
-        .registerBeanDefinition("doggo", beanDefinition);
   }
 }
